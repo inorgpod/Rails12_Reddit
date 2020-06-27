45c6c024706d972265901e4c139cd0e35eaba83a
Gao Ciaorun <itchynozz@gmail.com>
2020-06-27 18:55:41 +0800

    投票系統

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :links
+  resources :links do 
+    member do 
+      put "like", to: "links#vote"
+      put "dislike", to: "links#vote"
+    end
+  end
   root to: "links#index"
 end
